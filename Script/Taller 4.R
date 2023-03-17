7d65467d45b159b9b64bd70ac4dfe1c51ae870e9
LauraVannePardo <laurispardo2001@gmail.com>
2023-03-17 18:40:22 -0500

    Bigrams Word Cloud y otros ajustes

--- Script/Taller 4.R
+++ Script/Taller 4.R
@@ -20,11 +20,12 @@ head (train,1)
 
 #*************  Bloque de limpieza de datos *************
 
-train$text <- removeNumbers(train$text)
-train$text <- removePunctuation(train$text)
+train$text <- stri_trans_general(str = train$text, id = "Latin-ASCII")
+train$text <- gsub('[^A-Za-z0-9 ]+', ' ', train$text)
 train$text <- tolower(train$text)
-train$text <- stripWhitespace(train$text)
-train$text <- iconv(train$text, from = "UTF-8", to="ASCII//TRANSLIT")
+train$text <- gsub('\\s+', ' ', train$text)
+train$text <- gsub('\\d+', ' ', train$text)
+train$text <- trimws(train$text)
 
 head (train,1)
 
@@ -55,28 +56,74 @@ paste("La tasa de optimización fue de ->", tasa)
 
 wordcloud(tweet_text_clean$word, max.words=200, min.freq = 5, colors=brewer.pal(8,"Paired"))
 
-tweet_text_clean$stem <- stemDocument( tweet_text_clean$word, language="spanish")
-tweet_text_clean %>% 
-head()
+tweet_text_clean %>% group_by(name)%>% summarise(n=n())%>% 
+  ungroup() 
+  
+tweet_text_clean %>% group_by(name)%>% summarise(n=n())%>% 
+  ungroup() %>%
+  ggplot(aes(x = n)) +
+  geom_histogram(fill = "darkblue", alpha = 0.7) +
+  theme_bw() +
+  labs(x = "Número de tweets por personaje", y = "Cantidad")
+  
+
+
+# Tokenizacion
+tweet_original<-train$text
+head(tweet_original)
+tweet <- tokenize_words(train$text)
+head(tweet)
+
+stop_1 <- stopwords(language = "es", source = "snowball")
+stop_2 <- stopwords(language = "es", source = "nltk")
+stop <- union(stop_1, stop_2)
+stop
+
+tweet <- data.frame(matrix(tokenize_words(train$text)))
+colnames(tweet) <- c("token")
+tweet$token <- lapply(tweet$token, function(x) x[!x %in% stop])
+tweet$token[1]
+# Stem
+
+stem_tokens <- function(tokens) {
+  lapply(tokens, wordStem, language = "spanish")
+}
+
+tweet <- tweet %>%
+  mutate(stemmed_tokens = lapply(tweet$token, stem_tokens))
+colnames(tweet) <- c("col1","col2")
+  tweet$col2 <- lapply(tweet$col2, unlist)
+  tweet$col2
+
+
+#wordcloud(tweet$col1, max.words=200, min.freq = 20, colors=brewer.pal(8,"Paired"))
 
 #En R, podemos usar la función unnest_tokens() del paquete tidytext para generar n-grams a partir de un texto.
 
 
-bigrams <- as.data.frame(tweet_text_clean$word) %>%
-  unnest_tokens(bigram, tweet_text_clean$word, token = "ngrams", n = 2)
+bigrams_tweet <- as.data.frame(tweet_original) %>%
+  unnest_tokens(bigram, tweet_original , token = "ngrams", n = 2)
 
-bigrams
+bigrams_tweet
 
-stop_words <- data.frame(word1 = stopwords("es"), 
-                         word2 = stopwords("es"))
+stop_words <- data.frame(word1 = stop, 
+                         word2 = stop)
+
+# Eliminar los bigramas que contengan palabras de parada
+bigrams_tweet <- bigrams_tweet %>%
+  separate(bigram, c("word1", "word2"), sep = " ") %>%
+  anti_join(stop_words, by = "word1") %>%
+  anti_join(stop_words, by = "word2") %>%
+  unite(bigram, word1, word2, sep = " ")
 
 
 # Calcular la frecuencia de los bigramas
-word_freq <- tweet_text_clean %>%
-  count(tweet_text_clean$word, sort = TRUE)
+bigram_freq <- bigrams_tweet %>%
+  count(bigram, sort = TRUE)
 
-
-
-
-
-
+# Visualizar los bigramas más frecuentes
+ggplot(bigram_freq[1:10, ], aes(y = reorder(bigram, -n), x = n)) +
+  geom_bar(stat = "identity", fill = "#4e79a7") +
+  ggtitle("Bigramas más frecuentes") +
+  ylab("Bigramas") +
+  xlab("Frecuencia")
